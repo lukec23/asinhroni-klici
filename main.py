f636106ea74366b6256265f6cf24b2fd36f11dc6
lukec23 <luka.marjanovic@sts.sckr.si>
2026-02-24 10:35:05 +0000

    asdaw

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import random
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/coinflip")
+def coinflip():
+    return render_template("coinflip.html")
+
+@app.route("/coinData")
+def coinData():
+    #print(request.form) v primeru POST
+    #print(request.args["vrednost"])
+    status = random.choice(["HEADS","TAILS"])
+    imgSource={"HEADS" : "https://i.postimg.cc/CBNJNfDJ/head.png", "TAILS" : "https://i.postimg.cc/zysdXN8w/tail.png"}
+
+    return {"status" : status, "slika": imgSource[status]}
+
+@app.route("/quote")
+def coinflip():
+    return render_template("quote.html")
+
+@app.route("/quote")
+def coinData():
+    quotes=["Biti drugačen ni napaka, ampak moč.",
+            "Sreča ni nekaj, kar je pripravljeno. Sreča prihaja iz vaših dejanj.",
+            "Za vse, kar potrebujemo, je ljubezen. Vse ostalo je odveč.",
+            "Ne čakajte, da priložnosti pridejo k vam. Ustvarite jih sami.",
+            "Tisti, ki si upajo sanjati, so tisti, ki lahko spremenijo svet.",
+            "Edini način, da narediš dobro delo, je, da ljubiš to, kar počneš.",
+            "Ne glej na uro; počni, kar počne ura.",
+            "Učimo se iz preteklosti, a živimo v sedanjosti.",
+            "Sreča ni cilj, temveč pot.",
+            "Pomembno je, da si upamo. Da se postavimo, ko je treba."
+            ]
+
+    return 
+
+
+app.run(debug=True)
